b7d1604a8d1ce3e8f20f18233273b08beb9a0116
= <=>
2022-07-08 18:49:24 -0300

    Adding the delete user and get all users route in the backend, Starting the categories design in the frontend

--- client/src/data.js
+++ client/src/data.js
@@ -8,16 +8,37 @@ export const SliderItems = [
     },
     {
         id: 2,
-        img: "https://cdn-fnknc.nitrocdn.com/jwqHRGAzpUgGskUSHlppNQzwuXgXIKwg/assets/static/optimized/rev-e6f9055/wp-content/uploads/2021/12/luca-faloni-denim-shirt.jpeg",
+        img: "https://static05.jockey.in/uploads/dealimages/6509/zoomimages/shanghai-red-sport-t-shirt-2714-15.jpg",
         title: "Red T-shirt",
         desc: "50% off only for this summer!!",
         bg: "f5f1ed" 
     },
     {
         id: 3,
-        img: "https://cdn-fnknc.nitrocdn.com/jwqHRGAzpUgGskUSHlppNQzwuXgXIKwg/assets/static/optimized/rev-e6f9055/wp-content/uploads/2021/12/luca-faloni-denim-shirt.jpeg",
+        img: "https://blacktie.com.br/wp-content/uploads/2020/11/smoking1.png",
         title: "Black Tie",
         desc: "50% off only for this summer!!",
         bg: "f5f0f4" 
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://cdn-fnknc.nitrocdn.com/jwqHRGAzpUgGskUSHlppNQzwuXgXIKwg/assets/static/optimized/rev-e6f9055/wp-content/uploads/2021/12/luca-faloni-denim-shirt.jpeg",
+        title: "Blue Shirt"
+        
+    },
+    {
+        id: 2,
+        img: "https://static05.jockey.in/uploads/dealimages/6509/zoomimages/shanghai-red-sport-t-shirt-2714-15.jpg",
+        title: "Red T-shirt"
+        
+    },
+    {
+        id: 3,
+        img: "https://blacktie.com.br/wp-content/uploads/2020/11/smoking1.png",
+        title: "Black Tie"
+        
     }
 ]
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {verifyToken, verifyAuthorization} = require("./verifyToken");
+const {verifyToken, verifyAuthorization, verifyAdmin} = require("./verifyToken");
 const CryptoJs = require("crypto-js");
 require("dotenv").config();
 const User = require("../models/User");
@@ -17,6 +17,71 @@ router.put("/:id", verifyAuthorization, async (req, res) => {
     }catch(err){
         res.status(500).json(err)
     }
+})
+
+router.delete("/:id", verifyAuthorization, async (req, res) => {
+    try{
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User Deleted")
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+router.get("/find/:id", verifyAdmin, async (req, res) => {
+    try{
+        const user = await User.findById(req.params.id);
+        const {password, ...others} = user._doc;
+        res.status(200).json(others)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+router.get("/", verifyAdmin, async (req, res) => {
+
+    const query = req.query.new;
+
+    try{
+        const users = query ? await User.find().sort({_id: -1}).limit(1) : await User.find(req.params.id);
+        res.status(200).json(users)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+router.get("/stats", verifyAdmin , async (req, res) => {
+
+    const date = new Date();
+    const pastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+    try{
+        const data = await User.aggregate([
+            {$match: {
+                createdAt: {$gte: pastYear}
+            }},
+            {
+                $project: {
+                    month: {$month: "createdAt"},
+
+                }
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: {$sum: 1},
+                }
+            },
+        ]);
+        res.status(200).json(data)
+
+    }catch(err){
+        res.status(500).json(err)
+    }
+
 })
 
 module.exports = router
