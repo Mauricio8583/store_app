3992f090f85e87fd498838304bc54c7d679068e1
= <=>
2022-07-18 19:07:10 -0300

    Makeing the single product page design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Home from './pages/Home';
+import { Product } from './pages/Product';
 import { ProductList } from './pages/ProductList';
 
 export default function App() {
   return (
     <div>
-        <ProductList />
+        <Product />
     </div>
   )
 }
---
+++ client/src/pages/Product.jsx
@@ -0,0 +1,179 @@
+import { Add, Remove } from '@material-ui/icons'
+import React from 'react'
+import styled from 'styled-components'
+import { Annoucement } from '../components/Annoucement'
+import { Footer } from '../components/Footer'
+import { Navbar } from '../components/Navbar'
+import { Newsletter } from '../components/Newsletter'
+
+const Container = styled.div`
+
+
+`
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+
+`
+
+const ImgContainer = styled.div`
+    flex: 1;
+
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0px 50px;
+
+`
+
+const Title = styled.h1`
+    font-weight: 200;
+
+`
+
+const Desc = styled.p`
+    margin: 20px 0px;
+
+`
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+
+`
+
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 30px 0px;
+
+`
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+
+`
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+
+`
+
+const FilterColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.bg};
+    margin: 0px 5px;
+    cursor: pointer;
+
+`
+
+const FilterSize = styled.select`
+    margin-left: 10px;
+    padding: 5px;
+
+`
+
+const FilterSizeOption = styled.option`
+
+
+`
+
+const AddContainer = styled.div`
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   width: 50%;
+
+`
+
+const AmountContainer = styled.div`
+   display: flex;
+   align-items: center;
+   font-weight: 700;
+`
+
+const Amount = styled.span`
+   width: 30px;
+   height: 30px;
+   border-radius: 10px;
+   border: 1px solid teal;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   margin: 0px 5px;
+
+`
+
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    cursor: pointer;
+    font-weight: 500;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
+
+`
+
+export const Product = () => {
+  return (
+     <Container>
+        <Annoucement />
+        <Navbar />
+        <Wrapper>
+          <ImgContainer>
+            <Image src='https://lzd-img-global.slatic.net/g/p/d836e36abc3ae2a4597bd380f24a658e.jpg_720x720q80.jpg_.webp' />
+          </ImgContainer>
+          <InfoContainer>
+             <Title>Title</Title>
+             <Desc>Simple Description</Desc>
+             <Price>$ 20</Price>
+          </InfoContainer>
+          <FilterContainer>
+            <Filter>
+                <FilterTitle>Color</FilterTitle>
+                <FilterColor bg="black"></FilterColor>
+                <FilterColor bg="darkblue"></FilterColor>
+                <FilterColor bg="lightgreen"></FilterColor>
+            </Filter>
+            <Filter>
+                <FilterTitle>Size</FilterTitle>
+                <FilterSize>
+                    <FilterSizeOption>XS</FilterSizeOption>
+                    <FilterSizeOption>S</FilterSizeOption>
+                    <FilterSizeOption>M</FilterSizeOption>
+                    <FilterSizeOption>L</FilterSizeOption>
+                    <FilterSizeOption>XL</FilterSizeOption>
+                </FilterSize>
+            </Filter>
+
+          </FilterContainer>
+          <AddContainer>
+             <AmountContainer>
+                <Remove />
+                <Amount>1</Amount>
+                <Add />
+             </AmountContainer>
+             <Button>Add to cart</Button>
+          </AddContainer>
+        </Wrapper>
+        <Newsletter />
+        <Footer />
+     </Container>
+  )
+}
